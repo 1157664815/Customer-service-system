a102e7e12aa3800295e713a47d808f0adc5b0ec1
free Worry <1157664815@qq.com>
2019-11-07 22:42:07 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import ElementUI from 'element-ui';
-import VueI18n from 'vue-i18n';
-import { messages } from './components/common/i18n';
+
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
 // import './assets/css/theme-green/index.css'; // 浅绿色主题
 import './assets/css/icon.css';
@@ -11,16 +10,13 @@ import './components/common/directives';
 import 'babel-polyfill';
 
 Vue.config.productionTip = false;
-Vue.use(VueI18n);
+
 Vue.use(ElementUI, {
     size: 'small'
 });
-const i18n = new VueI18n({
-    locale: 'zh',
-    messages
-});
 
-//使用钩子函数对路由进行权限跳转
+
+//使用钩子函数对路由进行权限跳转 to（去向）我们要访问的路由  from（来源）路由从哪里跳转到哪里  next 下一步的选择
 router.beforeEach((to, from, next) => {
     document.title = `${to.meta.title} | vue-manage-system`;
     const role = localStorage.getItem('ms_username');
@@ -41,8 +37,8 @@ router.beforeEach((to, from, next) => {
     }
 });
 
+
 new Vue({
     router,
-    i18n,
     render: h => h(App)
 }).$mount('#app');
